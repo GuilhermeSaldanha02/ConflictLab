7455eaf0a61cea81423c06a8020e37fad1398078
Rodrigogfernandes <rodrigo.guedes.f@gmail.com>
2025-12-15 21:02:06 -0300

    nada

--- src/core/app.js
+++ src/core/app.js
@@ -3,7 +3,6 @@ function startApp() {
   console.log("Payments inicializados");
   console.log("Notificações ativas");
   console.log("Settings carregados");
-  console.log("App iniciado");
 }
 
-atsrtApp();
+startApp();
