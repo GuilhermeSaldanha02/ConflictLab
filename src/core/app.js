e7f5be76c89315e2ab89cbe3ebcfe9e0f6974d6c
marcelojacint <marcelojacintoferreirq@gmial.com>
2025-12-16 20:59:27 -0300

    Revert "Merge pull request #3 from GuilhermeSaldanha02/feat/notifications"
    This reverts commit 2b612825a06a67da319ea6c3e3bb3d2a5c651df4, reversing
    changes made to a2d1f9c8e7afbe17827c426f07c64e9ab0f4d3af.

--- src/core/app.js
+++ src/core/app.js
@@ -1,7 +1,6 @@
 function startApp() {
-  console.log("App iniciado");
-  console.log("Payments inicializados");
+    console.log("App iniciado");
+    console.log("Payments inicializados");
   console.log("App iniciado");
   console.log("Auth carregado");
-  console.log("Settings carregados");
 }
